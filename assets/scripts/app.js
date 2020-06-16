1b11071d5f80e4bc466755a454311dc29de2ada9
Foysol Ahmed <foysol.sahi@gmail.com>
2020-06-16 18:24:02 +0600

    minor change

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,5 @@
-// Variables 
-const canvas = document.getElementById('canvas');
+// Variables
+const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const shakeBtn = document.querySelector(".buttons__shake");
 
@@ -10,9 +10,10 @@ let height = canvas.height;
 
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
-// Canvas 
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
+
+// Canvas
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
 ctx.lineWidth = moveAmount;
 
 ctx.strokeStyle = "#777";
@@ -22,7 +23,7 @@ ctx.moveTo(x, y);
 ctx.lineTo(x, y);
 ctx.stroke();
 
-// Functions 
+// Functions
 function keyHandler(ev) {
     if (ev.key.includes("Arrow")) {
         ev.preventDefault();
@@ -35,16 +36,16 @@ function drawing(key) {
     ctx.moveTo(x, y);
 
     switch (key) {
-        case 'ArrowUp':
+        case "ArrowUp":
             y -= moveAmount;
             break;
-        case 'ArrowDown':
+        case "ArrowDown":
             y += moveAmount;
             break;
-        case 'ArrowRight':
+        case "ArrowRight":
             x += moveAmount;
             break;
-        case 'ArrowLeft':
+        case "ArrowLeft":
             x -= moveAmount;
             break;
         default:
@@ -58,20 +59,31 @@ function clearCanvas() {
     canvas.classList.add("shake");
     ctx.clearRect(0, 0, width, height);
 
-    canvas.addEventListener("animationend", ()=>{
-        canvas.classList.remove("shake");
-    }, {once: true});
+    canvas.addEventListener(
+        "animationend",
+        () => {
+            canvas.classList.remove("shake");
+        },
+        { once: true }
+    );
 }
 
-setTimeout(()=>{
-    document.querySelector(".message").style.display = 'none';
+setTimeout(() => {
+    document.querySelector(".message").style.display = "none";
 }, 6000);
 
-// Event Listener 
-shakeBtn.addEventListener('click', clearCanvas);
+// Event Listener
+shakeBtn.addEventListener("click", clearCanvas);
 window.addEventListener("keydown", keyHandler);
-document.querySelector(".btn-up").addEventListener('click', drawing.bind(null, "ArrowUp"));
-document.querySelector(".btn-down").addEventListener('click', drawing.bind(null, "ArrowDown"));
-document.querySelector(".btn-left").addEventListener('click', drawing.bind(null, "ArrowLeft"));
-document.querySelector(".btn-right").addEventListener('click', drawing.bind(null, "ArrowRight"));
-
+document
+    .querySelector(".btn-up")
+    .addEventListener("click", drawing.bind(null, "ArrowUp"));
+document
+    .querySelector(".btn-down")
+    .addEventListener("click", drawing.bind(null, "ArrowDown"));
+document
+    .querySelector(".btn-left")
+    .addEventListener("click", drawing.bind(null, "ArrowLeft"));
+document
+    .querySelector(".btn-right")
+    .addEventListener("click", drawing.bind(null, "ArrowRight"));
